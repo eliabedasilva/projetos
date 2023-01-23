7950557b7e13884a06d9ca9ce2aa09879c82decd
eliabedasilva <107951092+eliabedasilva@users.noreply.github.com>
2023-01-23 11:48:40 -0300

    sunyside finalizado

--- sunyside/script.js
+++ sunyside/script.js
@@ -5,3 +5,21 @@ button.addEventListener('click', ()=>{
   ulMobile.classList.toggle('activated')
 }
 )
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+link.addEventListener('click', event => {
+    event.preventDefault();
+
+    const targetId = event.target.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const topOffset = targetElement.offsetTop;
+
+
+    window.scrollTo({
+    top: topOffset,
+    behavior: 'smooth'
+    });
+});
+});
